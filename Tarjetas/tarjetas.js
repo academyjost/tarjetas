c4d6506f7f5ef676b5e038fe7ae34c5adfab63ff
academyjost <academyjost@gmail.com>
2026-05-02 21:53:50 -0500

    git add . git commit -m "Agregando archivos de tarjetas" git push origin main

---
+++ Tarjetas/tarjetas.js
@@ -0,0 +1,9 @@
+function crearTarjetas() {
+    let contenido = "";
+    let divTarjetas = document.getElementById("divTarjetas");
+    for(let i=1 ;i<=5;i++){
+        contenido = contenido + `<div class="item">` + i + `</div>`;
+        divTarjetas.innerHTML = contenido;
+    }
+    divTarjetas.innerHTML = contenido;
+}
